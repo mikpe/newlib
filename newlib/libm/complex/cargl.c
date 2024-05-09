ba4e8449304cde26d0d4c70876df4f0e9c489a89
Mikael Pettersson <mikpelinux@gmail.com>
2024-05-09 13:55:45 +0200

    cargl: only compile if needed callees are declared

--- newlib/libm/complex/cargl.c
+++ newlib/libm/complex/cargl.c
@@ -7,10 +7,21 @@
 #include <complex.h>
 #include <math.h>
 
+#undef HAVE_atan2l
+#if defined (_LDBL_EQ_DBL) || defined (__CYGWIN__) ||			\
+	defined(__aarch64__) || defined(__i386__) || defined(__x86_64__) || \
+	defined(__riscv)
+#ifndef _REENT_ONLY
+#ifndef __math_68881
+#define HAVE_atan2l
+#endif
+#endif
+#endif
+
 long double
 cargl(long double complex z)
 {     
-       #ifdef _LDBL_EQ_DBL
+       #if defined _LDBL_EQ_DBL || !defined HAVE_atan2l
          return carg (z);
        #else
          return atan2l (cimagl (z), creall (z));
