91e6e0fc0db3cba1eecd7241f6a57f41889069c7
Mikael Pettersson <mikpelinux@gmail.com>
2024-05-09 14:14:57 +0200

    csinhl: only compile if needed callees are declared

--- newlib/libm/complex/csinhl.c
+++ newlib/libm/complex/csinhl.c
@@ -32,6 +32,22 @@
 #include <complex.h>
 #include <math.h>
 
+#undef HAVE_sincosl
+#undef HAVE_sincoshl
+#if defined (_LDBL_EQ_DBL) || defined (__CYGWIN__) ||			\
+	defined(__aarch64__) || defined(__i386__) || defined(__x86_64__) || \
+	defined(__riscv)
+#ifndef __math_68881
+#define HAVE_sincosl
+#endif
+#ifndef _REENT_ONLY
+#ifndef __math_68881
+#define HAVE_sincoshl
+#endif
+#endif
+#endif
+
+#if defined HAVE_sincosl && defined HAVE_sincoshl
 long double complex
 csinhl(long double complex z)
 {
@@ -43,3 +59,4 @@ csinhl(long double complex z)
 	w = sinhl(x) * cosl(y) + (coshl(x) * sinl(y)) * I;
 	return w;
 }
+#endif
